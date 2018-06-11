de2cb47d3a00387317306892c993784b64369fc7
alvintian <alvintian2003@yahoo.ca>
2018-06-11 10:47:12 +0000

    too easy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+	if (fromN === toN) {
+		return fromN;
+	}
+	return fromN + sum(fromN + 1, toN);
+	// Sum all the values from fromN up to toN
 }
 
 module.exports = sum;
